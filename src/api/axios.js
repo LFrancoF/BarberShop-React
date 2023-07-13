98d045c7758ae83345d56aad4de6f4fd8e4bdf70
LFrancoF <gustavo.15.franco.fl@gmail.com>
2023-07-13 03:22:07 -0400

    .env added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+const API_URL = import.meta.env.VITE_API_REST || 'http://localhost:3030'
 const instance = axios.create({
-    baseURL: 'http://localhost:3030/api',
+    baseURL: `${API_URL}/api`,
     withCredentials : true
 })
 
