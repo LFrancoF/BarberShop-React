fb685a5a1e36cb53ab449a1c9f8a210c8f5a5bc0
LFrancoF <gustavo.15.franco.fl@gmail.com>
2023-07-13 08:37:35 -0400

    header axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_API_REST || 'http://localhost:3030/api',
-    withCredentials : true
+    withCredentials : true,
+    headers: {
+        "Content-type": "application/json",
+    },
 })
 
 export default instance
